f353a086c5117d4f19953050959050783ffa43cd
novakvova <novakvova@gmail.com>
2019-02-26 20:44:50 +0200

    Begin work site ASP.NET Core

---
+++ ClientApp/src/components/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store/Counter';
+
+const Counter = props => (
+  <div>
+    <h1>Counter</h1>
+
+    <p>This is a simple example of a React component.</p>
+
+    <p>Current count: <strong>{props.count}</strong></p>
+
+        <button onClick={props.increment}>Increment</button>
+        <button onClick={props.decrement}>Decrement</button>
+  </div>
+);
+
+//function mapStateToProps(state) {
+//    return { state: state.counter };
+//}
+
+export default connect(
+  state => state.counter,
+  dispatch => bindActionCreators(actionCreators, dispatch)
+)(Counter);
