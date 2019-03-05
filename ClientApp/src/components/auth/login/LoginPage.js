100c4fe9b5695dad526266d1d3ee8b3affa16930
novakvova <novakvova@gmail.com>
2019-03-05 18:59:27 +0200

    Begin work login form

---
+++ ClientApp/src/components/auth/login/LoginPage.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Login Form</h1>
+                
+            </div>
+        );
+    }
+}
+export default LoginPage;
