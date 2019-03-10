9444f2942af9b0b131e6c54f1c5bbaef23d52e83
arikatamo25 <arikatamo25@gmail.com>
2019-03-10 17:33:00 +0200

    Tags - Complete Add to database,errors

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -5,70 +5,42 @@ import { Glyphicon, Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "./NavMenu.css";
 
-class NavMenu extends Component {
-  state = {  }
-  render() { 
-    const props=this.props;
-    const {isAuthenticated, user} = this.props.auth;
-        console.log(isAuthenticated);
-
-        const userLinks = (
-          <LinkContainer to={"/login"}>
-            <NavItem>
-             <Glyphicon glyph="th-list" /> {user.name} Logout
-            </NavItem>
-          </LinkContainer>
-        );
-
-        const guestLinks = (
-          <LinkContainer to={"/login"}>
-            <NavItem>
-              <Glyphicon glyph="th-list" /> Login Form
-            </NavItem>
-          </LinkContainer>
-        );
-    return (
-      <Navbar inverse fixedTop fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to={"/"}>WebSiteCore</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to={"/"} exact>
-              <NavItem>
-                <Glyphicon glyph="home" /> Home
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={"/counter"}>
-              <NavItem>
-                <Glyphicon glyph="education" /> Counter
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={"/fetchdata"}>
-              <NavItem>
-                <Glyphicon glyph="th-list" /> Fetch data
-              </NavItem>
-            </LinkContainer>
-            {isAuthenticated ? userLinks : guestLinks}
-            <LinkContainer to={"/users"}>
-              <NavItem>
-                <Glyphicon glyph="th-list" /> Users
-              </NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
-}
-
-const mapStateToProps=(state)=>{
-  return {
-      auth: state.auth
-  };
-}
- 
-export default connect(mapStateToProps, null)(NavMenu);
+export default props => (
+  <Navbar inverse fixedTop fluid collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to={"/"}>WebSiteCore</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <LinkContainer to={"/"} exact>
+          <NavItem>
+            <Glyphicon glyph="home" /> Home
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to={"/counter"}>
+          <NavItem>
+            <Glyphicon glyph="education" /> Counter
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to={"/fetchdata"}>
+          <NavItem>
+            <Glyphicon glyph="th-list" /> Fetch data
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to={"/login"}>
+          <NavItem>
+            <Glyphicon glyph="th-list" /> Login Form
+          </NavItem>
+        </LinkContainer>
+        <LinkContainer to={"/users"}>
+          <NavItem>
+            <Glyphicon glyph="th-list" /> Users
+          </NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
