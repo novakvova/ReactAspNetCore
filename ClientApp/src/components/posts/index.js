4d3866e93a63a4a00faf182ff512fa018a64e0e3
Soul_Reaver <jepik_ak15@nuwm.edu.ua>
2019-03-13 19:15:16 +0200

    Init post element

---
+++ ClientApp/src/components/posts/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostElement extends Component {
+    render(){
+        return (
+            <div>
+                Hello;
+            </div>
+        )
+    }
+}
+
+export default PostElement;
