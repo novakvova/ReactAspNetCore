f3d3958fc668295d76872a936769ebd0889097ee
novakvova <novakvova@gmail.com>
2019-03-27 17:41:08 +0200

    register and login is good

--- ClientApp/src/actions/authActions.js
+++ ClientApp/src/actions/authActions.js
@@ -23,13 +23,7 @@ export function login(data) {
     return dispatch => {
         return axios.post('api/Account/login', data)
         .then(res => {
-            var token=res.data;
-            //console.log("data login", token);
-            var user=jwt.decode(token);
-            //console.log('-----user login------', user);
-            localStorage.setItem('jwtToken', token);
-            setAuthorizationToken(token);
-            dispatch(setCurrentUser(user));
+          loginByJWT(res.data, dispatch);
         });
     }
 }
@@ -38,21 +32,24 @@ export function socialLogin(data) {
   return dispatch => {
     return axios.post('api/Account/socialLogin', data)
       .then(res => {
-        var token=res.data;
-            var user=jwt.decode(token);
-            localStorage.setItem('jwtToken', token);
-            setAuthorizationToken(token);
-            dispatch(setCurrentUser(user));
+        loginByJWT(res.data, dispatch);
       })
   }
 }
+const loginByJWT = (token, dispatch) => {
+  var user=jwt.decode(token);
+  localStorage.setItem('jwtToken', token);
+  setAuthorizationToken(token);
+  dispatch(setCurrentUser(user));
+} 
 
 export function register(data) {
     console.log('--data--', data);
     return dispatch => {
         return axios.post('api/Account/Register', data)
             .then(res => {
-                console.log("data register", res);
+                //console.log("data register", res);
+                loginByJWT(res.data, dispatch);
             });
     }
 }
